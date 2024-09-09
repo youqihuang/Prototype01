dceba84ef3dbe63e3174095d5e149ad8cd06179a
Mary Karapetyan <mekarape@usc.edu>
2024-09-09 16:02:06 -0700

    Added speed effect to sprite

---
+++ Assets/SpriteEffect.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SpriteEffect : MonoBehaviour
+{
+    private ParticleSystem lightningSpeed; 
+    private bool isMoving = false;
+
+    void Start()
+    {
+        lightningSpeed = GetComponentInChildren<ParticleSystem>();
+        if (lightningSpeed == null)
+        {
+            Debug.LogError("No ParticleSystem found in children.");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (!isMoving)
+            {
+                lightningSpeed.Play();
+                isMoving = true;
+            }
+        }
+        else
+        {
+            if (isMoving)
+            {
+                lightningSpeed.Stop();
+                isMoving = false;
+            }
+        }
+    }
+}
