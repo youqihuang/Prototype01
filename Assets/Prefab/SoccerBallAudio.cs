fa523637e630d9758f3d6381856f50586a0181c8
Mary Karapetyan <mekarape@usc.edu>
2024-09-09 16:18:11 -0700

    Added bounce sound during ball collision

---
+++ Assets/Prefab/SoccerBallAudio.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBallAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+         audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the ball collided with the sprite
+        if (collision.gameObject.CompareTag("SoccerSprite"))
+        {
+            // Play the bounce sound
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
